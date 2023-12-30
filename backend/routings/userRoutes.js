fcd61726c6e0c7834252ffcd8885468ed921ad91
rajgopalreddyand <rajgade061@gmail.com>
2023-12-30 21:38:48 +0530

    added validations to the signup page

--- backend/routings/userRoutes.js
+++ backend/routings/userRoutes.js
@@ -2,19 +2,49 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-//const transporter = require('../utils/nodemailer'); // Import the nodemailer configuration
+const validator = require('validator');
 
 router.post('/register', async (req, res) => {
     try {
-        const { username, email, password, firstName, lastName, mobileNumber, altMobileNumber } = req.body;
+        const { 
+            username, 
+            email, 
+            password, 
+            firstName, 
+            lastName, 
+            mobileNumber, 
+            altMobileNumber, 
+            dateOfBirth,  // Assuming dateOfBirth is a new field
+            address  // Assuming address is a new field
+        } = req.body;
 
-        // Check if the username or email is already taken
+        // Validate email format
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ message: 'Invalid email format' });
+        }
+
+        // Validate password strength
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+        }
+
+        // Validate mobile number
+        if (!/^\d{10}$/.test(mobileNumber)) {
+            return res.status(400).json({ message: 'Mobile Number must have exactly 10 digits' });
+        }
+
+        // Validate date of birth (example validation)
+        if (!validator.isISO8601(dateOfBirth)) {
+            return res.status(400).json({ message: 'Invalid date of birth format' });
+        }
+
+        // Additional validations for other fields can be added here
+
         const existingUser = await User.findOne({ $or: [{ username }, { email }] });
         if (existingUser) {
             return res.status(400).json({ message: 'Username or email already taken' });
         }
 
-        // Create a new user with 'isVerified' set to false
         const newUser = new User({
             username,
             email,
@@ -23,12 +53,13 @@ router.post('/register', async (req, res) => {
             lastName,
             mobileNumber,
             altMobileNumber,
+            dateOfBirth,
+            address,
             isVerified: false
         });
 
         await newUser.save();
 
-        // Generate a JWT for the newly registered user
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         // Send verification email (uncomment and complete this section if needed)
@@ -48,7 +79,6 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         console.error('Error during registration:', error);
 
-        // Provide more specific error messages based on the type of error
         if (error.name === 'ValidationError') {
             return res.status(400).json({ message: 'Validation error. Please check your input.' });
         }
